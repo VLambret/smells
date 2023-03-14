1a879cbb256ec9f54e8bcdf6a263cb30d47c25ba
Martial Monneret <martial.monneret@sogilis.com>
2023-03-14 11:56:07 +0100

    export to the expected json format output (with fake values)

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,5 +1,8 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+use std::collections::HashMap;
 
 #[derive(Debug, StructOpt)]
 pub struct CmdArgs{
@@ -7,18 +10,80 @@ pub struct CmdArgs{
     path: PathBuf,
 }
 
+#[derive(Deserialize, Serialize)]
+enum FileType{
+    File(FileEntry),
+    Folder(FolderEntry),
+}
+
+#[derive(Deserialize, Serialize)]
+struct Metrics{
+    lines_metrics: u32
+}
+
+#[derive(Deserialize, Serialize)]
+struct FileEntry{
+    metrics: Metrics
+}
+
+#[derive(Deserialize, Serialize)]
+struct FolderEntry{
+    metrics: Metrics,
+    folder_content: HashMap<String, FileType>,
+}
+
 pub fn smells(){
     let args = CmdArgs::from_args();
-    print_analysis(args.path);
+    //print_analysis(args.path).expect("Error");
+
+    let file1_metrics = Metrics{
+        lines_metrics: 0,
+    };
+
+    let file2_metrics = Metrics{
+        lines_metrics: 2,
+    };
+
+    let file1_entry = FileEntry{
+        metrics: file1_metrics,
+    };
+
+    let file2_entry = FileEntry{
+        metrics: file2_metrics,
+    };
+
+    let folder1_metrics = Metrics{
+        lines_metrics: 0,
+    };
+
+    let mut folder1_entry = FolderEntry{
+        metrics: folder1_metrics,
+        folder_content: HashMap::new(),
+    };
+
+    let mut json_data = HashMap::new();
+    json_data.insert("file1".to_string(), FileType::File(file1_entry));
+    folder1_entry.folder_content.insert("file2".to_string(), FileType::File(file2_entry));
+    json_data.insert("folder1".to_string(), FileType::Folder(folder1_entry));
+
+    let mut file = std::fs::File::options()
+                        .read(true)
+                        .write(true)
+                        .create(true)
+                        .open("json_output_test_file.json").unwrap();
+
+    let output = serde_json::to_string(&json_data).unwrap();
+    file.write(output.as_bytes()).unwrap();
 }   
 
-fn print_analysis(analysed_file: PathBuf){
+fn print_analysis(analysed_file: PathBuf) -> Result<(),Box<dyn std::error::Error>> {
     let file_os_str = analysed_file.as_os_str();
     let file_key = match analysed_file.file_name() {
         Some(file_name) => file_name.to_owned(),
         _ => file_os_str.to_owned(),
     };
 
+    let file_line_number = 0;
     let json_output_with_empty_folder = format!(
     r#"{{
         {:?}: {{
@@ -29,13 +94,13 @@ fn print_analysis(analysed_file: PathBuf){
         }}
     }}"#, file_key);
 
-    let folder_content = 
+    let folder_content = format!(
     r#"
-                "file0.txt": {
-                    "metrics": {
-                        "lines_metric": 0
-                    }
-                }"#;
+                {:?}: {{
+                    "metrics": {{
+                        "lines_metric": {}
+                    }}
+                }}"#, "file0.txt",file_line_number);
 
     let json_output_with_folder_not_empty = format!(
     r#"{{
@@ -59,4 +124,5 @@ fn print_analysis(analysed_file: PathBuf){
     else{
         print!("{}", json_output_with_empty_folder);
     }
+    Ok(())
 }
