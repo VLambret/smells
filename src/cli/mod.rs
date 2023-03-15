29949853e2d5358083a35dcdacb3ddd4ea232269
Martial Monneret <martial.monneret@sogilis.com>
2023-03-15 16:01:39 +0100

    FIX: revert JSON related code to make tests pass again

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,8 +1,5 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
-use serde::{Deserialize, Serialize};
-use std::io::Write;
-use std::collections::HashMap;
 
 #[derive(Debug, StructOpt)]
 pub struct CmdArgs{
@@ -10,70 +7,9 @@ pub struct CmdArgs{
     path: PathBuf,
 }
 
-#[derive(Deserialize, Serialize)]
-enum FileType{
-    File(FileEntry),
-    Folder(FolderEntry),
-}
-
-#[derive(Deserialize, Serialize)]
-struct Metrics{
-    lines_metrics: u32
-}
-
-#[derive(Deserialize, Serialize)]
-struct FileEntry{
-    metrics: Metrics
-}
-
-#[derive(Deserialize, Serialize)]
-struct FolderEntry{
-    metrics: Metrics,
-    folder_content: HashMap<String, FileType>,
-}
-
 pub fn smells(){
     let args = CmdArgs::from_args();
-    //print_analysis(args.path).expect("Error");
-
-    let file1_metrics = Metrics{
-        lines_metrics: 0,
-    };
-
-    let file2_metrics = Metrics{
-        lines_metrics: 2,
-    };
-
-    let file1_entry = FileEntry{
-        metrics: file1_metrics,
-    };
-
-    let file2_entry = FileEntry{
-        metrics: file2_metrics,
-    };
-
-    let folder1_metrics = Metrics{
-        lines_metrics: 0,
-    };
-
-    let mut folder1_entry = FolderEntry{
-        metrics: folder1_metrics,
-        folder_content: HashMap::new(),
-    };
-
-    let mut json_data = HashMap::new();
-    json_data.insert("file1".to_string(), FileType::File(file1_entry));
-    folder1_entry.folder_content.insert("file2".to_string(), FileType::File(file2_entry));
-    json_data.insert("folder1".to_string(), FileType::Folder(folder1_entry));
-
-    let mut file = std::fs::File::options()
-                        .read(true)
-                        .write(true)
-                        .create(true)
-                        .open("json_output_test_file.json").unwrap();
-
-    let output = serde_json::to_string(&json_data).unwrap();
-    file.write(output.as_bytes()).unwrap();
+    print_analysis(args.path).expect("Error");
 }   
 
 fn print_analysis(analysed_file: PathBuf) -> Result<(),Box<dyn std::error::Error>> {
