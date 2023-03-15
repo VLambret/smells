64f52dd8e68680c40006e7a43d2c3e52a7278005
Martial Monneret <martial.monneret@sogilis.com>
2023-03-15 16:58:36 +0100

    REFACTOR: reorganize analysis steps

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -7,30 +7,41 @@ pub struct CmdArgs{
     path: PathBuf,
 }
 
+pub struct Analysis {
+    analysed_file: PathBuf
+}
+
 pub fn smells(){
     let args = CmdArgs::from_args();
-    print_analysis(args.path).expect("Error");
+    do_analysis(args.path).expect("Error");
 }   
 
-fn print_analysis(analysed_file: PathBuf) -> Result<(),Box<dyn std::error::Error>> {
-    let file_os_str = analysed_file.as_os_str();
-    let file_key = match analysed_file.file_name() {
+fn do_analysis(analysed_file: PathBuf) -> Result<(),Box<dyn std::error::Error>> {
+
+    let file_line_number = 0;
+
+    let analysis = Analysis{
+        analysed_file
+    };
+    print_analysis(analysis);   
+    Ok(())
+}
+
+fn print_analysis(analysis: Analysis) {
+    let file_os_str = analysis.analysed_file.as_os_str();
+    let file_key = match analysis.analysed_file.file_name() {
         Some(file_name) => file_name.to_owned(),
         _ => file_os_str.to_owned(),
     };
 
-    let file_line_number = 0;
     let json_output_with_empty_folder = format!(
-    r#"{{
+        r#"{{
         {:?}: {{
             "metrics": {{
                 "lines_metric": 0
             }},
             "folder_content": {{}}
         }}
-    }}"#, file_key);
-
-    print!("{}", json_output_with_empty_folder);
-    
-    Ok(())
+    }}"#, file_key);  
+    print!("{}", json_output_with_empty_folder)
 }
