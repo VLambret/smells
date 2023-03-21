90c37e79e386a9fc467110f5fae97d1fe2810b93
Martial Monneret <martial.monneret@sogilis.com>
2023-03-21 16:26:54 +0100

    REFACTOR: change print_analysis and definition of structs to handle vectors of content

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -10,14 +10,14 @@ pub struct CmdArgs{
     path: PathBuf,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct AnalysisResult{
     file: String,
     metrics: Metrics,
-    file_content: Option<Box<AnalysisResult>>
+    file_content: Option<Vec<AnalysisResult>>
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Metrics{
     lines_count: u32
 }
@@ -29,8 +29,7 @@ pub fn smells() -> Result<()> {
 }  
 
 fn do_analysis(file: PathBuf) -> Result<()> {
-    let analysis = analyse(file);
-    print_analysis(analysis)?;
+    print_analysis(analyse(file))?;
     Ok(())
 }
 
@@ -49,21 +48,22 @@ fn analyse(folder: PathBuf) -> AnalysisResult{
         lines_count: get_file_line_metrics(&folder)
     };
 
+    let mut vec_content = Vec::new();
 
     AnalysisResult{
         file: extract_key(&folder),
         metrics,
-        //file_content: Some(Box::new(file_content))
         file_content: if file_is_empty(&folder) || file_is_current_folder(folder){
             None
         } else {
-            Some(Box::new(file_content))
+            vec_content.push(file_content);
+            Some(vec_content)
         },
     }
 }
 
 fn file_is_current_folder(file: PathBuf) -> bool{
-    if let Ok(path) = file.read_dir(){
+    if let Ok(_path) = file.read_dir(){
         let mut path_to_compare = PathBuf::new();
         path_to_compare.push(".");
         if file.to_path_buf() == path_to_compare{
@@ -88,28 +88,6 @@ fn extract_key(file: &PathBuf) -> String{
         _ => file_os_str.to_owned(),
     };
     file_key.to_string_lossy().into_owned()
-}
-
-fn extract_file_content(file: &PathBuf) -> String{
-    // create the file content if needed to be displayed in print_analysis
-    let mut file_content = "".to_string();
-
-    if let Ok(mut path) = file.read_dir(){
-        let mut path_to_compare = PathBuf::new();
-        path_to_compare.push(".");
-        
-        if !path.next().is_none() && file.to_path_buf() != path_to_compare{
-            let files_name = extract_files_name(&file);
-            let lines_metric = get_file_line_metrics(&file);
-            file_content = format!(
-            r#""{}": {{
-                "metrics": {{
-                    "lines_metric": {}
-                }}
-            }}"#, files_name, lines_metric);
-        }
-    }
-    file_content
 }
 
 fn extract_files_name(file: &PathBuf) -> String{
@@ -156,34 +134,34 @@ fn format_analysis_to_json(file: String, line_count_metric: u32, folder_content:
         }}"#, file, line_count_metric, folder_content)
 }
 
-fn print_formatted_json(json_output: String) -> Result<()>{
-    let converted_json_output: Value = serde_json::from_str(&json_output)?;
-    print!("{}", serde_json::to_string_pretty(&converted_json_output)?);
-    Ok(())
-}
-
 fn print_analysis(analysis: AnalysisResult) -> Result<String>{
     let file_key = analysis.file;
     let lines_metric = analysis.metrics.lines_count;
     let file_content = analysis.file_content;
     let mut converted_file_content = "".to_string();
 
-    if let Some(content) = file_content {
-        let mut file_content_string = String::new();
-
-
-        if let Some(inner_content) = content.file_content {
-            let inner_result = print_analysis(*inner_content)?;
-            file_content_string.push_str(&inner_result);
+    if let Some(contents) = file_content {
+        for content in contents{
+            let mut file_content_string = String::new();
+            if let Some(inner_contents) = &content.file_content {
+                for inner_content in inner_contents.iter(){
+                    let inner_result = print_analysis(inner_content.clone())?;
+                file_content_string.push_str(&inner_result);
+                }
+            }
+            converted_file_content = format_analysis_to_json(content.file.to_string(), content.metrics.lines_count, file_content_string);
         }
-
-        converted_file_content = format_analysis_to_json(content.file.to_string(), content.metrics.lines_count, file_content_string);
     }
-
     let folder_content = format!(
     r#","folder_content": [{}]"#, converted_file_content);
         
     let json_output = format_analysis_to_json(file_key, lines_metric, folder_content);
     print_formatted_json(json_output.clone())?;
     Ok(json_output)
+}
+
+fn print_formatted_json(json_output: String) -> Result<()>{
+    let converted_json_output: Value = serde_json::from_str(&json_output)?;
+    print!("{}", serde_json::to_string_pretty(&converted_json_output)?);
+    Ok(())
 }
