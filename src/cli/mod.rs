5739b794fa3e6ea3884867f82ff033552739b1fe
Martial Monneret <martial.monneret@sogilis.com>
2023-03-24 14:50:12 +0100

    REFACTOR: modify build_json_metrics to use json library

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,9 +1,10 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
-use serde_json::{Value};
+use serde_json::{Value, json};
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 use serde::{Serialize, Deserialize};
+
 #[derive(Debug, StructOpt)]
 pub struct CmdArgs{
     #[structopt(default_value=".")]
@@ -17,7 +18,7 @@ struct AnalysisResult{
     folder_content: Option<Vec<AnalysisResult>>
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
 struct Metrics{
     lines_count: u32
 }
@@ -32,29 +33,47 @@ fn do_analysis(item: PathBuf){
 }
 
 fn analyse(item: PathBuf) -> AnalysisResult{
-    let metrics_content = Metrics{
-        lines_count: compute_lines_count_metric(&item)
-    };
-
-    let file_content1 = AnalysisResult{
-        item_key: extract_file_name_of_analysed_folder(&item),
-        metrics: metrics_content,
-        folder_content: None
-    };
-
-    let metrics = Metrics {
-        lines_count: compute_lines_count_metric(&item)
-    };
 
     let mut folder_contents = Vec::new();
 
+
+    /*for entry in std::fs::read_dir(item.clone()).unwrap(){
+        let path = entry.unwrap().path();
+        println!("{:?}", path);
+        let metrics = Metrics{
+            lines_count: compute_lines_count_metric(&path)
+        };
+
+        let file = AnalysisResult{
+            item_key: extract_file_name(&path),
+            metrics,
+            folder_content: None
+        };
+
+        folder_contents.push(file);
+    }*/
+
+    let metrics = Metrics{
+        lines_count: compute_lines_count_metric(&item)
+    };
+
+    let file = AnalysisResult{
+        item_key: extract_file_name(&item),
+        metrics,
+        folder_content: None
+    };
+
+    let metrics_content = Metrics {
+    lines_count: compute_lines_count_metric(&item.clone())
+    };
+
     AnalysisResult{
         item_key: extract_analysed_item_key(&item),
-        metrics,
+        metrics: metrics_content,
         folder_content: if folder_is_empty(&item) || analysed_item_is_in_current_folder(&item){
             None
         } else {
-            folder_contents.push(file_content1);
+            folder_contents.push(file);
             Some(folder_contents)
         },
     }
@@ -87,9 +106,9 @@ fn extract_analysed_item_key(item: &PathBuf) -> String{
     item_key.to_string_lossy().into_owned()
 }
 
-fn extract_file_name_of_analysed_folder(folder: &PathBuf) -> String{
+fn extract_file_name(file: &PathBuf) -> String{
     let mut file_name = String::new();
-    if let Ok(files) = std::fs::read_dir(&folder) {
+    if let Ok(files) = std::fs::read_dir(&file) {
         for file in files {
             if let Ok(entry) = file {
                 file_name = extract_analysed_item_key(&entry.path());
@@ -103,7 +122,7 @@ fn compute_lines_count_metric(folder: &PathBuf) -> u32{
     let mut lines_count: u32 = 0;
     let mut current_path = PathBuf::new();
     current_path.push(".");
-    let file_name = extract_file_name_of_analysed_folder(&folder);
+    let file_name = extract_file_name(&folder);
     let file_to_analyse = folder.to_string_lossy().into_owned().to_string()+"/"+&file_name;
 
     match try_opening_file(file_to_analyse){
@@ -118,17 +137,17 @@ fn compute_lines_count_metric(folder: &PathBuf) -> u32{
             }
         }
         Err(e) => {
-            println!("{}", e);
+            eprintln!("{}", e);
         }
     }
     lines_count
 }
 
 fn try_opening_file(file_to_open: String) -> Result<File, String>{
-    let file = match File::open(file_to_open){
+    let file = match File::open(file_to_open.clone()){
         Ok(file) => file,
         Err(e) => {
-            return Err(format!("Can't open the file: {}", e));
+            return Err(format!("Can't open {}: {}", file_to_open, e));
         }
     };
     Ok(file)
@@ -149,29 +168,19 @@ fn build_json_item_analysis(item: &AnalysisResult) -> String {
     item_to_json
 }
 
-// build folder content array of the root folder
+// build json folder content of the root folder
 // -> "folder_content" : [XXX]
-fn build_root_folder_content_array(items: Vec<AnalysisResult>) -> String{
+fn build_root_folder_content(items: Vec<AnalysisResult>) -> String{
     items.iter().map(|item| build_json_item_analysis(item)).collect()
 }
 
-// build content for each folder in the root folder content array (recursive content)
+// build content for each folder in the json root folder content (recursive content)
 // -> folder_content [ folder2_content : [XXX] ]
 fn build_inner_folder_content(folder_contents: &Vec<AnalysisResult>) -> String{
     folder_contents.iter().map(|item| print_analysis(item.clone())).collect::<String>()
 }
 
 // build the content that is in folder content array
-/* Result is (if only one file):
-{
-    "file0.txt": {
-        "metrics": {
-            "lines_metric": 0
-        }
-    }
-}
-*/
-
 fn build_json_folder_analysis(folder: String, json_metrics: &String, folder_content: &String) -> String{
     // build analysis result json
     // build metrics
@@ -193,32 +202,26 @@ fn build_json_file_analysis(file: String, json_metrics: &String) -> String{
         }}"#, file, json_metrics)
 }
 
-fn build_json_metrics(metrics: &Metrics) -> String{
+fn build_json_metrics(metrics: &Metrics) -> String {
     // build analysis result json
-    // build metrics
-    format!(
-        r#"
-        "metrics": {{
-            "lines_metric": {}
-        }}
-        "#, metrics.lines_count)
+    let lines_metric = json!({"lines_metric": metrics.lines_count});
+    format!(r#" "metrics":{}"#, lines_metric)
 }
 
 // build analysis result in json
 fn build_json_result_analysis(analysis: AnalysisResult) -> String{
-    // build analysis result
     // build root item
     let item_key = analysis.item_key;
     let folder_content = analysis.folder_content;
-    let mut converted_file_content = String::new();
+    let mut folder_content_result = String::new();
     let json_metrics = build_json_metrics(&analysis.metrics);
 
     // build folder content
     if let Some(items) = folder_content {
-        converted_file_content = build_root_folder_content_array(items);
+        folder_content_result = build_root_folder_content(items);
     }
     let folder_content = format!(
-        r#","folder_content": [{}]"#, converted_file_content);
+        r#","folder_content": [{}]"#, folder_content_result);
     build_json_folder_analysis(item_key, &json_metrics, &folder_content)
 }
 
@@ -230,19 +233,21 @@ fn print_analysis(analysis: AnalysisResult) -> String{
 }
 
 fn print_formatted_json(json_output: &String){
+    // TODO: remonter le from_str
     match serde_json::from_str::<Value>(json_output){
         Ok(converted_json_output) => {
             match serde_json::to_string_pretty(&converted_json_output){
                 Ok(pretty_json) => {
                     print!("{}", pretty_json);
                 }
-                Err(e) => {
-                    println!("Error for printing JSON: {}", e);
+                Err(..) => {
+                    // if formatting fails we print the original version
+                    println!("{}", json_output);
                 }
             }
         }
         Err(e) => {
-            println!("Error for serializing JSON: {}", e);
+            eprintln!("Error for serializing JSON: {}", e);
         }
     }
 }
