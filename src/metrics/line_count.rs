a54c1fd32ef68e87492d448bdd05ee9d57d751fd
Martial Monneret <martial.monneret@sogilis.com>
2023-04-05 11:00:11 +0200

    TEST: add unit test for count_lines function

--- src/metrics/line_count.rs
+++ src/metrics/line_count.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
+use std::io::Read;
 use crate::analysis::models::Analysis;
 
 pub fn summary_lines_count_metric(folder_contents: &Vec<Analysis>) -> usize {
@@ -25,4 +25,22 @@ pub fn compute_lines_count_metric(file: &mut File) -> Result<usize, std::io::Err
 
 fn count_lines(content: String) -> usize {
     content.lines().count()
+}
+
+#[cfg(test)]
+mod tests{
+    use rstest::rstest;
+    use super::*;
+    #[rstest(input, expected,
+    case("", 0),
+    case("line1", 1),
+    case("line1\nline2", 2),
+    case("line1\nline2\nline3", 3),
+    case("\n", 1),
+    case("\n\n\n", 3)
+    )]
+    fn test_count_lines(input: &str, expected: usize) {
+        let line_count = count_lines(input.to_owned());
+        assert_eq!(line_count, expected);
+    }
 }
