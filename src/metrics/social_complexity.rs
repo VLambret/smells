c3e88b3b3fbc8a8f33634cd7aba372a63c5a8bca
Martial Monneret <martial.monneret@sogilis.com>
2023-04-06 15:27:49 +0200

    TEST: social_complexity unit test: add creation of a temp directory

--- src/metrics/social_complexity.rs
+++ src/metrics/social_complexity.rs
@@ -30,43 +30,37 @@ fn get_number_of_authors_of_a_file(_repo_path: &Repository, _file: &PathBuf) -> 
 
 #[cfg(test)]
 mod tests{
-    use git2::Oid;
     use super::*;
     use rstest::rstest;
     use tempdir::TempDir;
 
     #[rstest(file, expected_authors,
-    case("file1.txt", 1),  // file1.txt has 3 authors
+    case("file0.txt", 0),  // file1.txt has 3 authors
     )]
     fn smells_get_number_of_authors_of_a_file(file: &str, expected_authors: u32){
         // Create a temporary directory for testing
-        /*let temp_dir = TempDir::new("temp_folder").unwrap();
-        let temp_git_repo = temp_dir.path().join("test_repo");
-        eprintln!("{:?}", temp_git_repo);*/
+        let git_repo_path = "git_repo_test";
+        let temp_dir = TempDir::new("").unwrap();
+        let temp_git_repo = temp_dir.path().join(git_repo_path);
+
 
         // Create directory
-        let git_repo_path = "tests/git_repo_test";
-        std::fs::create_dir(git_repo_path).unwrap();
+        //let git_repo_path = "tests/git_repo_test";
+        //std::fs::create_dir(git_repo_path).unwrap();
 
         // Initialize a repository in the directory
-        let repo = Repository::init(&git_repo_path).unwrap();
+        let repo = Repository::init(temp_git_repo).unwrap();
 
         create_initial_commit(&repo);
         create_file(&repo, file);
         add_file_to_the_staging_area(&repo, file);
         commit_changes_to_repo(&repo);
 
-        // Commit the changes
-        /*let tree_id = commit_changes(&repo, oid);
-
         // Get the path of the committed file
-        let file_path = repo_path.join(file);
-
-        // Call the function to get the number of authors for the file
-        let actual_authors = get_number_of_authors_of_a_file(&repo, &file_path);
+        let committed_file_path = repo.path().join(file);
 
         // Assert that the actual number of authors matches the expected number of authors
-        assert_eq!(actual_authors, expected_authors);*/
+       assert_eq!(get_number_of_authors_of_a_file(&repo, &committed_file_path), expected_authors);
     }
 
     fn create_initial_commit(repo: &Repository) {
@@ -98,19 +92,12 @@ mod tests{
         index.write().unwrap();
     }
 
-    // create a branch with a specified name
-    fn create_branch(repo: &Repository, branch_name: &str){
-        let head = repo.head().unwrap();
-        let head_commit = repo.find_commit(head.target().unwrap()).unwrap();
-        repo.branch(branch_name, &head_commit, false).unwrap();
-    }
-
-    fn commit_changes_to_repo(repo: &Repository) -> Oid{
+    fn commit_changes_to_repo(repo: &Repository){
         let head = repo.head().unwrap();
         let parent = repo.find_commit(head.target().unwrap()).unwrap();
         let signature = repo.signature().unwrap();
         let tree = repo.find_tree(repo.index().unwrap().write_tree().unwrap()).unwrap();
-        let tree_id = repo.commit(
+        repo.commit(
             Some("HEAD"),
             &signature,
             &signature,
@@ -118,7 +105,6 @@ mod tests{
             &tree,
             &[&parent],
         ).unwrap();
-        tree_id
     }
 
 }
