8e0bdf9b89115f4f6ed7ef5bd323826139cad3d5
Martial Monneret <martial.monneret@sogilis.com>
2023-04-14 14:46:00 +0200

    TEST: file_system: refactor file_system function to collect all files and folders of a path

--- src/data_sources/file_system.rs
+++ src/data_sources/file_system.rs
@@ -1,33 +1,27 @@
 use std::collections::HashSet;
 use std::path::PathBuf;
 
-fn collect_files(path: &PathBuf, root: &PathBuf) -> HashSet<String> {
-    let mut files = HashSet::new();
-    for entry in std::fs::read_dir(path).unwrap() {
-        let entry_path = entry.unwrap().path();
-        if entry_path.is_file() {
-            let relative_file_path = entry_path.strip_prefix(root).unwrap().to_owned();
-            let full_file_path = relative_file_path.to_string_lossy().to_string();
-            files.insert(full_file_path);
-        } else if entry_path.is_dir() {
-            files.extend(collect_files(&entry_path, root));
-        }
-    }
-    files
+fn file_system(path: PathBuf) -> HashSet<PathBuf>{
+    let collection_of_files_and_folders_path = collect_entries(&path, &path);
+    println!("{:?}", collection_of_files_and_folders_path);
+    collection_of_files_and_folders_path
 }
 
-fn collect_directories(path: &PathBuf, root: &PathBuf) -> HashSet<String> {
-    let mut directories = HashSet::new();
+// TODO: handle unwrap()
+fn collect_entries(path: &PathBuf, root: &PathBuf) -> HashSet<PathBuf>{
+    let mut entries = HashSet::new();
     for entry in std::fs::read_dir(path).unwrap() {
         let entry_path = entry.unwrap().path();
-        if entry_path.is_dir() {
-            let relative_file_path = entry_path.strip_prefix(root).unwrap().to_owned();
-            let full_dir_path = relative_file_path.to_string_lossy().to_string();
-            directories.insert(full_dir_path);
-            directories.extend(collect_directories(&entry_path, root));
+        let relative_entry_path = entry_path.strip_prefix(root).unwrap().to_owned();
+        if entry_path.is_file() {
+            entries.insert(relative_entry_path);
+        }
+        else{
+            entries.insert(relative_entry_path);
+            entries.extend(collect_entries(&entry_path, root));
         }
     }
-    directories
+    entries
 }
 
 mod tests{
@@ -35,28 +29,17 @@ mod tests{
     const PATH_TO_DIR: &str = "tests/data/file_system";
 
     #[test]
-    fn walk_through_directories_and_get_files(){
+    fn test_file_system(){
+        let mut expected_output = HashSet::new();
+        expected_output.insert(PathBuf::from("file3.txt"));
+        expected_output.insert(PathBuf::from("subfolder1/file1.txt"));
+        expected_output.insert(PathBuf::from("subfolder2/file2.txt"));
+        expected_output.insert(PathBuf::from("folder1"));
+        expected_output.insert(PathBuf::from("subfolder1"));
+        expected_output.insert(PathBuf::from("subfolder2"));
+        expected_output.insert(PathBuf::from("folder1/folder_in_folder1"));
 
-        let mut expected_output_files = HashSet::new();
-        expected_output_files.insert("file3.txt".to_string());
-        expected_output_files.insert("subfolder1/file1.txt".to_string());
-        expected_output_files.insert("subfolder2/file2.txt".to_string());
-
-        let actual_files = collect_files(&PathBuf::from(PATH_TO_DIR), &PathBuf::from(PATH_TO_DIR));
-        println!("{:?}", actual_files);
-        assert_eq!(actual_files, expected_output_files);
-    }
-
-    #[test]
-    fn walk_through_directories_and_get_directories(){
-        let mut expected_output_dir = HashSet::new();
-        expected_output_dir.insert("folder1".to_string());
-        expected_output_dir.insert("subfolder1".to_string());
-        expected_output_dir.insert("subfolder2".to_string());
-        expected_output_dir.insert("folder1/folder_in_folder1".to_string());
-
-        let actual_dir = collect_directories(&PathBuf::from(PATH_TO_DIR), &PathBuf::from(PATH_TO_DIR));
-        println!("{:?}", actual_dir);
-        assert_eq!(actual_dir, expected_output_dir);
+        let actual_dir = file_system(PathBuf::from(PATH_TO_DIR));
+        assert_eq!(actual_dir, expected_output);
     }
 }
