5300a5c11055865238aacfead86a5e10f28c3cfc
LucasGautierPro <lucas.gautier@sogilis.com>
2023-08-09 11:10:43 +0200

    FIX: improve run.sh for performance tests

--- fs_generator/run.sh
+++ fs_generator/run.sh
@@ -2,6 +2,9 @@
 
 NUMBER_OF_FILES=$1
 
-./target/release/fs_generator.exe $NUMBER_OF_FILES 1 1 0
-time ../target/release/smells.exe ./root_directory/ > /dev/null
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+
+./target/release/fs_generator $NUMBER_OF_FILES 1 1 0
+time ../target/release/smells ./root_directory/ > /dev/null
 mv root_directory/ root_directory_"${NUMBER_OF_FILES}"
